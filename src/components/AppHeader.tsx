054e6bd1a93d58d3f84e723d64c531afce7f2e97
[dyad] <crusader.jatt@gmail.com>
2025-09-26 23:09:17 +0530

    [dyad] Set the logo's height to h-10 and maintained its aspect ratio in the AppHeader. - wrote 1 file(s)

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -29,7 +29,7 @@ const AppHeader: React.FC<AppHeaderProps> = ({ className }) => {
       className
     )}>
       <Link to="/" className="flex items-center space-x-2">
-        <img src="/SplitMyPDF_Logo.png" alt="Split My PDF Logo" /> {/* Removed h-12 to allow natural size */}
+        <img src="/SplitMyPDF_Logo.png" alt="Split My PDF Logo" className="h-10 w-auto" /> {/* Set height to h-10 and width to auto */}
       </Link>
 
       <nav className="flex items-center space-x-4">
